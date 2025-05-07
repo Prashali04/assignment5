5c72543a13f24ea907e13dea31cda72a026bbb2d
prashali kawale <prashalikawale2018@gmail.com>
2025-05-07 19:12:43 +0530

    hellostudent is added

---
+++ HelloStudent.java
@@ -0,0 +1,7 @@
+public class HelloStudent{
+	public static void  main(String[] args){
+		System.out.println("hello janvi");
+		System.out.println("hello prashali");
+		System.out.println("hello pradnya");
+	}
+}
